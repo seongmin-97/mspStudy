94eaad3eeeef292368a4b3ad718c4f0af91982ed
seongmin-97 <seongmin.tech@gmail.com>
2022-03-05 21:19:12 +0900

    rename

--- image_feature_detection/test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import pysift
-
-image = cv2.imread('./data/box.png', 0)
-print("입력 이미지의 데이터 타입 : ", type(image))
-keypoints, descriptors = pysift.computeKeypointsAndDescriptors(image)
-print("결과 keypoints 데이터 타입 : ", type(keypoints))
-print("결과 descriptors 데이터 타입 : ", type(descriptors))
